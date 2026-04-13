2d7b82366953c8f8304c55940b7938d404257c04
lutfi <lutfisallahudin05@gmail.com>
2026-04-13 16:23:47 +0800

    app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def predict():
             "error": str(e)
         })
 
-# IMPORTANT FOR DEPLOY
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
